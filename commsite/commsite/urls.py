61d47be916de98465a8a2967ff55790e13297848
AndySmithQA <andy.smtih@qa.com>
2023-01-17 12:43:32 +0000

    Link to detail view

--- commsite/commsite/urls.py
+++ commsite/commsite/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('<int:id>/',views.detail,name="detail")
 ]
