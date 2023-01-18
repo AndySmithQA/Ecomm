c18551a128174ea046000f04d27c923c851019a8
AndySmithQA <andy.smtih@qa.com>
2023-01-18 12:00:16 +0000

    prices for each individual item

--- commsite/commsite/urls.py
+++ commsite/commsite/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-    path('<int:id>/',views.detail,name="detail")
+    path('<int:id>/',views.detail,name="detail"),
+    path('checkout/',views.checkout,name="checkout")
 ]
